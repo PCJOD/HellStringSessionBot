a8f9bd64a936d84539346799abad9d29822b71d8
Pc <126058657+PCJOD@users.noreply.github.com>
2024-02-12 14:41:46 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -9,7 +9,7 @@ from StringGen.utils import add_served_user, keyboard, keyboard2
 async def f_start(_, message: Message):
     await message.reply_photo(photo="https://telegra.ph/file/7fc57533c40ff57d070c3.jpg", caption=f" ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ (ᴇᴠᴇɴ ᴠᴇʀꜱɪᴏɴ 2) ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ. \n\nᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.\n\n๏ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴇᴀᴛᴜʀᴇs ✨\n────────────────────\n\n➲ /start : ꜱᴛᴀʀᴛꜱ ᴍᴇ | ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍᴇ ʏᴏᴜ'ᴠᴇ ᴀʟʀᴇᴀᴅʏ ᴅᴏɴᴇ ɪᴛ.\n➲ /help  : ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ.\n──────────────────── ",
         reply_markup=keyboard,
-        disable_web_page_preview=True,
+        disable_web_page_preview=False,
     )
     await add_served_user(message.from_user.id)
 
